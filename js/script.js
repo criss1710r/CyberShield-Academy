df4106a6ef583020a8d56f28b85b7166e3322c00
Cristofher Rengifo <criss1710r@gmail.com>
2026-09-24 12:39:57 -0500

    Version inicial de CyberShield Academy

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function startLearning() {
+
+    document
+        .getElementById("ramas")
+        .scrollIntoView({
+            behavior: "smooth"
+        });
+
+}
+
+
+function exploreBranches() {
+
+    document
+        .getElementById("ramas")
+        .scrollIntoView({
+            behavior: "smooth"
+        });
+
+}
+
+
+console.log(
+    "%cCyberShield Academy",
+    "color:#00ff9d;font-size:20px;font-weight:bold;"
+);
+
+console.log(
+    "%cEducational Cybersecurity Platform",
+    "color:#35e8ff;font-size:13px;"
+);
